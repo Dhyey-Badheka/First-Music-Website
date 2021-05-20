317f33176185c03b1187c52a354c12bc46213646
Dhyey Badheka <75579801+Dhyey-Badheka@users.noreply.github.com>
2021-05-20 22:02:52 +0530

    Add files via upload

---
+++ js/landing.js
@@ -0,0 +1,21 @@
+
+$(function () {
+    menu = $('nav ul');
+
+    $('#openup').on('click', function (e) {
+        e.preventDefault(); menu.slideToggle();
+    });
+
+    $(window).resize(function () {
+        var w = $(this).width(); if (w > 580 && menu.is(':hidden')) {
+            menu.removeAttr('style');
+        }
+    });
+
+    $('nav li').on('click', function (e) {
+        var w = $(window).width(); if (w < 580) {
+            menu.slideToggle();
+        }
+    });
+    $('.open-menu').height($(window).height());
+});
